595a63f5c4a50cc976c0ddb7c0ca00e91fb43d08
Jaroslav Bisikirski <kazunekit@gmail.com>
2013-07-20 20:18:28 +0200

    Settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,58 +1,71 @@
 # Django settings for app project.
 import os
 
-PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-DEBUG = True
+ON_OPENSHIFT = False
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+	ON_OPENSHIFT = True
+
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+if ON_OPENSHIFT:
+	DEBUG = False
+else:
+	DEBUG = True
+#DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+AUTH_USER_MODEL = 'votes.User'
+
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+	# ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
 
 if os.environ.has_key('OPENSHIFT_POSTGRESQL_DB_HOST'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get('OPENSHIFT_APP_NAME'),
-            'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
-            'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
-            'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
-            'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT'),
-        }
-    }
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql_psycopg2',
+			'NAME': os.environ.get('OPENSHIFT_APP_NAME'),
+			'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+			'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+			'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
+			'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT'),
+		}
+	}
 elif os.environ.has_key('OPENSHIFT_MYSQL_DB_HOST'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('OPENSHIFT_APP_NAME'),
-            'USER': os.environ.get('OPENSHIFT_MYSQL_DB_USERNAME'),
-            'PASSWORD': os.environ.get('OPENSHIFT_MYSQL_DB_PASSWORD'),
-            'HOST': os.environ.get('OPENSHIFT_MYSQL_DB_HOST'),
-            'PORT': os.environ.get('OPENSHIFT_MYSQL_DB_PORT'),
-        }
-    }
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.mysql',
+			'NAME': os.environ.get('OPENSHIFT_APP_NAME'),
+			'USER': os.environ.get('OPENSHIFT_MYSQL_DB_USERNAME'),
+			'PASSWORD': os.environ.get('OPENSHIFT_MYSQL_DB_PASSWORD'),
+			'HOST': os.environ.get('OPENSHIFT_MYSQL_DB_HOST'),
+			'PORT': os.environ.get('OPENSHIFT_MYSQL_DB_PORT'),
+		}
+	}
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', PROJECT_DIR), 'sqlite3.db'),
-        }
-    }
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', PROJECT_DIR), 'sqlite3.db'),
+		}
+	}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    os.environ.get('OPENSHIFT_GEAR_DNS'),
+#	 os.environ.get('OPENSHIFT_GEAR_DNS'),
 ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+if 'OPENSHIFT_DATA_DIR' in os.environ:
+	TIME_ZONE = 'America/New_York'
+else:
+	TIME_ZONE = 'Europe/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -92,39 +105,39 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	# Put strings here, like "/home/html/static" or "C:/www/django/static".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#	 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'hz@!kzw4j)pit1)girk19rf%=dv=66ruzplj@5v6i1_%#47yx0'
+SECRET_KEY = '^m!25susge$6#@lotjd2v1$11zx+3pn40wxcgz%bihjbavi54k'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+#	  'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
+	'django.middleware.cache.UpdateCacheMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	# Uncomment the next line for simple clickjacking protection:
+	# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
 ROOT_URLCONF = 'app.urls'
@@ -132,68 +145,90 @@ ROOT_URLCONF = 'app.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'app.wsgi.application'
 
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+	TEMPLATE_DIR = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'), 'app', 'votes', 'templates')
+else:
+	TEMPLATE_DIR = os.path.join(os.environ.get('PROJECT_ROOT'), 'wsgi', 'voting', 'votes', 'templates')
+
+
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages',
-    'django.core.context_processors.request',
+	'django.contrib.auth.context_processors.auth',
+	'django.core.context_processors.debug',
+	'django.core.context_processors.i18n',
+	'django.core.context_processors.media',
+	'django.core.context_processors.static',
+	'django.core.context_processors.tz',
+	'django.contrib.messages.context_processors.messages',
+	'django.core.context_processors.request',
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	# Uncomment the next line to enable the admin:
+	# 'django.contrib.admin',
+	# Uncomment the next line to enable admin documentation:
+	# 'django.contrib.admindocs',
+	'constance.backends.database',
+	'constance',
+	'crispy_forms',
+	'votes'
 )
 
+CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+
+CONSTANCE_CONFIG ={
+	'VOTE_TIME': (7, 'Time in days for voting'),
+}
+
+LOGIN_URL='/login'
+
+USE_L10N = False
+DATETIME_FORMAT='m/d/Y H:i'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True,
+		},
+	}
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': ':'.join([os.environ.get('OPENSHIFT_INTERNAL_IP', '127.0.0.1'), '18080']),
-    }
+	'default': {
+		'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+		'LOCATION': ':'.join([os.environ.get('OPENSHIFT_INTERNAL_IP', '127.0.0.1'), '18080']),
+	}
 }
