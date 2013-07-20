188daf56db7e466f64e25dc2a087ec3cc205c2e9
Jaroslav Bisikirski <kazunekit@gmail.com>
2013-07-20 21:22:34 +0200

    remove urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,11 +16,11 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 )
 
-from django.conf import settings
-if settings.DEBUG:    
-	urlpatterns += patterns('', (r'^media\/(?P<path>.*)$',
-									 'django.views.static.serve',
-									 {'document_root': settings.MEDIA_ROOT}),
-							   )
-	from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-	urlpatterns += staticfiles_urlpatterns()
+#from django.conf import settings
+#if settings.DEBUG:    
+#	urlpatterns += patterns('', (r'^media\/(?P<path>.*)$',
+#									 'django.views.static.serve',
+#									 {'document_root': settings.MEDIA_ROOT}),
+#							   )
+#	from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#	urlpatterns += staticfiles_urlpatterns()
